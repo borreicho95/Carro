892142699a63946f8ebf147322423edb87993352
gabriel <200100291@esg.ipsantarem.pt>
2021-03-12 16:17:13 +0000

    Foram criados comentários e gerada a decomentação JavaDoc

--- src/Carro.java
+++ src/Carro.java
@@ -1,9 +1,19 @@
-
+/**
+ * A classe carro contém métodos para realizar operações:
+ * <ul>
+ * 		<li>Meter Combustivel<li>
+ * 		<li>O carro andar<li>
+ * 		<li>O carro parar<li>
+ * 		<li>Ver a autonomia do carro<li>
+ * @author gabri
+ *
+ */
 public class Carro {
-	
+	//Consumo do carro (km/l)
 	static private final double consumo=5.5; // litros por 100 klm
-	
+	//Reabastecer, neste momento é 0
 	private double quantidadeCombustivel;
+	//Combustivel que o carro leva
 	private double capacidadeDeposito;
 	
 	// Construtor por defeito 
@@ -29,6 +39,10 @@ public class Carro {
 	}
 	
 	// Métodos 
+	/**
+	 * Adiciona combustivel ao carro.
+	 * @param quantidade
+	 */
 	
 	public void meterCombustivel( double quantidade) { 		
 		
@@ -39,6 +53,10 @@ public class Carro {
 		
 	}
 	
+	/**
+	 * Vê quantos litros encheu.
+	 * @return Número de litros postos
+	 */
 	// atestar o depósito
 	public double meterCombustivel() {
 		
@@ -50,7 +68,11 @@ public class Carro {
 		return litrosMeter;
 	}
 	
-	
+	/**
+	 * Carro anda até ter combustivel.
+	 * @param distancia
+	 * @return Se o carro anda ou fica parado.
+	 */
 	// distancia em klm
 	public boolean andar( double distancia ) {
 		
@@ -76,7 +98,10 @@ public class Carro {
 		// falta a flag que indica se o carro está em andamento
 		return true;
 	}
-	
+	/**
+	 * Vê autonomia.
+	 * @return autonomia.
+	 */
 	// kmCombustivel( )    
 	public double obterAutonomia() {
 		
